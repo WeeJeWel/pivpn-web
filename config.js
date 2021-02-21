3a10ad4bbb2f071c62d6148e3a2aab250fa842cf
Emile Nijssen <emile@emilenijssen.nl>
2021-02-21 15:42:45 +0100

    update env

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports.PORT = process.env.PORT || 51821;
-module.exports.SSH_HOST = process.env.SSH_HOST || '127.0.0.1';
+module.exports.SSH_HOST = process.env.SSH_HOST || '172.17.0.1';
 module.exports.SSH_PORT = Number(process.env.SSH_PORT) || 22;
