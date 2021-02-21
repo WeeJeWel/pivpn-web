056eed571174d26cde183b70d80b95027c733979
Emile Nijssen <emile@emilenijssen.nl>
2021-02-21 12:52:14 +0100

    rewrite to ssh

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 'use strict';
 
 module.exports.PORT = process.env.PORT || 51821;
+module.exports.SSH_HOST = process.env.SSH_HOST || '192.168.1.2';
+module.exports.SSH_PORT = Number(process.env.SSH_PORT) || 22;
--- services/PiVPNWireGuard.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const PiVPNWireGuard = require('../lib/PiVPNWireGuard');
-
-module.exports = new PiVPNWireGuard();
